104103bbc380acedd06a4759c5109d07e8d3a4f9
“DongLieu” <duongduydongtq@gmail.com>
2024-10-18 17:33:13 +0700

    suc

--- ibc-gaia-onomy/chain1.sh
+++ ibc-gaia-onomy/chain1.sh
@@ -1,7 +1,7 @@
 rm -rf $HOME/.onomy/
 
 # init chain
-onomyd init test --chain-id onomy-1
+onomyd init test --chain-id testing-1
 
 echo $(cat /Users/donglieu/script/keys/mnemonic1)| onomyd keys add val --keyring-backend test  --recover 
 echo $(cat /Users/donglieu/script/keys/mnemonic2)| onomyd keys add val2 --keyring-backend test  --recover 
@@ -12,7 +12,7 @@ onomyd genesis add-genesis-account val 10000000000000000000000000000000stake --k
 onomyd genesis add-genesis-account val2 10000000000000000000000000000000stake --keyring-backend test
 
 
-onomyd genesis gentx val  1000000000000000000000000000000stake --keyring-backend test --chain-id onomy-1
+onomyd genesis gentx val  1000000000000000000000000000000stake --keyring-backend test --chain-id testing-1
 # # Collect genesis tx 
 onomyd genesis collect-gentxs
 # # Run this to ensure everything worked and that the genesis file is setup correctly
@@ -21,4 +21,4 @@ onomyd genesis validate-genesis
 
 sed -i -E 's|minimum-gas-prices = ""|minimum-gas-prices = "0.0001stake"|g' $HOME/.onomy/config/app.toml
 # screen -S xionx -t xionx -d -m
-onomyd start
+# onomyd start
--- ibc-gaia-onomy/run.sh
+++ ibc-gaia-onomy/run.sh
@@ -3,6 +3,10 @@ killall onomyd || true
 killall gaiad || true
 killall rly || true
 
+# init genesis 2 chain
+/Users/donglieu/script/ibc-gaia-onomy/chain1.sh
+/Users/donglieu/script/ibc-gaia-onomy/chain2.sh
+
 echo "Running chain 1"
 screen -S onomy -t onomy -d -m onomyd start 
 
@@ -10,22 +14,31 @@ echo "Running chain 2"
 screen -S gaia -t gaia -d -m gaiad start --pruning=nothing  --minimum-gas-prices=0.0001stake 
 
 sleep 7
+# setup rly
+/Users/donglieu/script/ibc-gaia-onomy/rly.sh
 
+sleep 7
 echo "Running relayer"
-screen -S relayerd -t relayerd -d -m rly start --home /Users/donglieu/script/ibc-chainnel/rly
-# ./rly.sh
+screen -S relayerd -t relayerd -d -m rly start --home /Users/donglieu/script/ibc-gaia-onomy/rly
 
-sleep 7
-
-gaiad tx ibc-transfer transfer transfer channel-1 $(onomyd keys show val --keyring-backend test -a)  10000stake --from val --chain-id testing-2 --yes --keyring-backend test --gas 6000000 --fees 6000000stake --node tcp://127.0.0.1:26654
-
-sleep 7
-
-echo "Query balances..."
+echo "Query balances1..."
 echo "Balances gaia:"
 
 gaiad q bank balances $(gaiad keys show val --keyring-backend test -a) --node tcp://127.0.0.1:26654
 
-echo "Balances onomy:"
-onomyd q bank balances $(onomyd keys show mykey --keyring-backend test -a) 
+echo "Balances onomy1:"
+onomyd q bank balances $(onomyd keys show val --keyring-backend test -a) 
+sleep 7
+
+gaiad tx ibc-transfer transfer transfer channel-0 $(onomyd keys show val --keyring-backend test -a)  10000stake --from val --chain-id testing-2 --yes --keyring-backend test --gas 6000000 --fees 6000000stake --node tcp://127.0.0.1:26654
+
+sleep 14
+
+echo "Query balances2..."
+echo "Balances gaia:"
+
+gaiad q bank balances $(gaiad keys show val --keyring-backend test -a) --node tcp://127.0.0.1:26654
+
+echo "Balances onomy2:"
+onomyd q bank balances $(onomyd keys show val --keyring-backend test -a) 
 
