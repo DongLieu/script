d880baeb8322e48ea9245cda65d84cd6639e907a
Đông Liều <duongduydongtq@gmail.com>
2025-10-13 15:38:23 +0700

    bcnad chain

--- bitcana/mainnet/run.sh
+++ bitcana/mainnet/run.sh
@@ -1,15 +1,17 @@
+# Downloads  from https://snapshots.polkachu.com/snapshots/bitcanna/bitcanna_21630181.tar.lz4
+
 # snapshot -> data
-lz4 -c -d /Users/donglieu/Downloads/bitcanna_21615190.tar.lz4 | tar -x -C $HOME/.bcna/
+lz4 -c -d /Users/donglieu/Downloads/bitcanna_21630181.tar.lz4 | tar -x -C $HOME/.bcna/
 
 # genesis.json
 curl -s https://snapshots.polkachu.com/genesis/bitcanna/genesis.json > $HOME/.bcna/config/genesis.json
 
-# path binary
-cd /Users/donglieu/1025/bitcanna/bcna/
-go install ./...
-chaind=bcnad
+# # path binary
+# cd /Users/donglieu/1025/bitcanna/bcna/
+# go install ./...
+# chaind=bcnad
 
-$chaind init test --chain-id testing-1
+# $chaind init test --chain-id testing-1
 
 
-/Users/donglieu/1025/cosmos-tooling/test-data/
+# /Users/donglieu/1025/cosmos-tooling/test-data/
