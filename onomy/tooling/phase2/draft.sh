43ed8fcdbc2950653bc6fee6d950c2eb54b546ae
Đông Liều <duongduydongtq@gmail.com>
2025-10-20 13:19:49 +0700

    ok

--- onomy/tooling/phase2/draft.sh
+++ onomy/tooling/phase2/draft.sh
@@ -1,1 +1,5 @@
+echo "turtle wrist awful way output atom return vehicle seminar sudden door breeze solve visit arm globe valid seat real found border toward useful logic" | onomyd keys add validator7 --recover --keyring-backend=test --home=$HOME/.onomy
+
+sleep 7
+
 onomyd tx gov submit-proposal  /Users/donglieu/script/onomy/tooling/phase2/draft_proposal.json --keyring-backend=test  --home=$HOME/.onomy --from onomy1ujyr90wqkqpgp5rlfrvcgagyrwfqdpq9v90eh0 -y --chain-id testing-1 --fees 200stake --gas 401627
