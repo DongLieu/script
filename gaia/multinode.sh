aa1050a81b786e6978177a5d79675a5a164ae527
Đông Liều <duongduydongtq@gmail.com>
2025-11-10 16:01:47 +0700

    done

--- gaia/multinode.sh
+++ gaia/multinode.sh
@@ -102,13 +102,14 @@ sed -i -E 's|prometheus = false|prometheus = true|g' $VALIDATOR3_CONFIG
 sed -i -E 's|prometheus_listen_addr = ":26660"|prometheus_listen_addr = ":26620"|g' $VALIDATOR3_CONFIG
 
 # # update
-# update_test_genesis () {
-#     # EX: update_test_genesis '.consensus_params["block"]["max_gas"]="100000000"'
-#     cat $HOME/.gaiad/validator1/config/genesis.json | jq "$1" > tmp.json && mv tmp.json $HOME/.gaiad/validator1/config/genesis.json
-#     cat $HOME/.gaiad/validator2/config/genesis.json | jq "$1" > tmp.json && mv tmp.json $HOME/.gaiad/validator2/config/genesis.json
-#     cat $HOME/.gaiad/validator3/config/genesis.json | jq "$1" > tmp.json && mv tmp.json $HOME/.gaiad/validator3/config/genesis.json
-# }
+update_test_genesis () {
+    # EX: update_test_genesis '.consensus_params["block"]["max_gas"]="100000000"'
+    cat $HOME/.gaiad/validator1/config/genesis.json | jq "$1" > tmp.json && mv tmp.json $HOME/.gaiad/validator1/config/genesis.json
+    cat $HOME/.gaiad/validator2/config/genesis.json | jq "$1" > tmp.json && mv tmp.json $HOME/.gaiad/validator2/config/genesis.json
+    cat $HOME/.gaiad/validator3/config/genesis.json | jq "$1" > tmp.json && mv tmp.json $HOME/.gaiad/validator3/config/genesis.json
+}
 
+update_test_genesis '.app_state["feemarket"]["params"]["max_block_utilization"] = "30000000000"'
 # update_test_genesis '.app_state["gov"]["voting_params"]["voting_period"] = "30s"'
 # update_test_genesis '.app_state["mint"]["params"]["mint_denom"]= "stake"'
 # update_test_genesis '.app_state["gov"]["deposit_params"]["min_deposit"]=[{"denom": "stake","amount": "1000000"}]'
@@ -143,14 +144,28 @@ sleep 7
 # gaiad tx bank send $(gaiad keys show validator1 -a --keyring-backend=test --home=$HOME/.gaiad/validator1) $(gaiad keys show validator2 -a --keyring-backend=test --home=$HOME/.gaiad/validator2) 100000stake --keyring-backend=test --chain-id=testing-1 -y --home=$HOME/.gaiad/validator1 --fees 10stake
 
 # sleep 7
-gaiad tx wasm store /Users/donglieu/1125/contract-test/contract/artifacts/fib_verifier.wasm --keyring-backend=test --home=$HOME/.gaiad/validator1 --from validator1 -y --chain-id testing-1 --gas 1481605 --fees 9962080stake
+# gaiad tx wasm store /Users/donglieu/1125/sp1/contract-fib/artifacts/fib_verifier.wasm --keyring-backend=test --home=$HOME/.gaiad/validator1 --from validator1 -y --chain-id testing-1 --gas 1927006 --fees 9962080stake
+
+gaiad tx wasm store /Users/donglieu/script/gaia/fib_verifier.wasm --keyring-backend=test --home=$HOME/.gaiad/validator1 --from validator1 -y --chain-id testing-1 --gas 1927006 --fees 9962080stake
 
 
-
+# /Users/donglieu/1125/sp1/contract-fib/artifacts/fib_verifier.wasm
 sleep 7
 # gaiad tx bank send gaia1f7twgcq4ypzg7y24wuywy06xmdet8pc4hhtf9t gaia16gjg8p5fedy48wf403jwmz2cxlwqtkqlwe0lug 10000000000000000000000stake --keyring-backend=test --chain-id=testing-1 -y --home=$HOME/.gaiad/validator1 --fees 10stake
 
-gaiad tx wasm instantiate 1 '{"max_n":null}' \
+# gaiad tx wasm instantiate 1 '{"max_n":null}' \
+#   --label fib-verifier \
+#   --no-admin \
+#   --from validator1 \
+#   --home=$HOME/.gaiad/validator1 \
+#   --chain-id testing-1 \
+#   --keyring-backend=test \
+#   --gas auto --gas-adjustment 1.3 --fees 2043077stake \
+#   -y
+
+
+gaiad tx wasm instantiate 1 \
+  '{"max_n":null,"verifying_key":"rU2ap+MC2d9BdJ1VB5SdBdvqM/uxbGQ7IvWZor5t8uLhoVdcLklNNhPpXkO2IjGNkiXIIORqzQjoyYe0QFEZW8lnAy/L93bRr8mF+Ih38YLThICmU/Leyql5TLw78wYMDhh4R61MeYN00NZzK/UBhH3Wi8DgcSQeAhO8f8E9t6uZjpOTkg1IOnJgv7cx+10l8apJMzWp5xKX5IW3rvMSwhgA3u8SHx52QmoAZl5cRHlnQyLU917a3UbevVzZkvbt2OVzmnPWV+gyozZ5GXczKkuW5bv9y5kDr+SH25qmy13cx8uN5xVnXyHwHsybRtI24IZeDMAgAkUhmYJphF905gP/QfS6DDf+LK8nNU0o5Lj4PTt2d3pjsyfXNr/7ASLtAAAAA6YJHhyvsK2KTqCmlM03Q+v1JHeSM9tzTEUdKLWKqXWOhhw/0P09ol0mB8In0JDMp1DtNsbsh4dV5TfBxIlR+0yE6rJBOIp5gX/g4OLq0LLsT/3sUaFgKN7gIGNP0SnnHAAAAAAAAAAA", "sp1_vkey_hash":"0x0010146cc27b7fa97cbeeccab84112bd0df9b965902208efbbf827e408f8fabd"}' \
   --label fib-verifier \
   --no-admin \
   --from validator1 \
@@ -161,10 +176,15 @@ gaiad tx wasm instantiate 1 '{"max_n":null}' \
   -y
 
 
-sleep 7
-
-gaiad query wasm contract-state smart cosmos14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s4hmalr '{"expected":{"n":8}}' 
 
 sleep 7
 
-gaiad tx wasm execute cosmos14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s4hmalr '{"verify":{"n":8,"value":"22"}}' --keyring-backend=test --home=$HOME/.gaiad/validator1 --from validator1 -y --chain-id testing-1 --gas 416005 --fees 417080stake
+payload='{"verify":{"n":20,"value":"6765","proof":"pFlMWQ0YaF3byDn9wHguclvwm9erJLdVCjjuxyDOViPJinvnKQEJkR7qrDk9/c4AEg3SS6qWc7RfCJK2yByPcdA1h3wb1oHXsPL07nMfkBm44kajXQobBnzJhZoriJ/M1Mz+KhNCgk6w6C+0yFaCS0RAgfs/5hV6kgv6NP8aHZu2RQoKKXsZdLAMUKA/lnpOoABlnjtOz496rgO5MGbp57/5DwYiaxZvsEXybOTRAKIfzDUH0mQNV5kt3KYM7smsB8/IJRySG3EDu77Z4s/VLco+aFe+9hSSNZKnbtX8quqWZTWJBdDB8ku1FjN7CXMXX3Dt+T/1/1WRxqSXM1B7hOD7ZQU="}}'
+
+gaiad tx wasm execute cosmos14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s4hmalr "$payload" \
+  --from validator1 \
+  --home=$HOME/.gaiad/validator1 \
+  --chain-id testing-1 \
+  --keyring-backend=test \
+  --gas 300000000 --fees 3000000000stake \
+  -y
